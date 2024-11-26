0fbe0bbf76d39dcadb15d8919f89e7901be17a55
Maximilien Brunet <maximilien.brunet@me.com>
2024-11-26 23:23:56 +0100

    added structure for background video

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,42 @@
 import Navbar from "./Navbar";
+import styled from "styled-components";
+import BackgroundVideo from "../assets/BackgroundVideo.mp4";
 
 export default function Header() {
   return (
-    <div>
+    <HeaderContainer>
+      <VideoContainer>
+        <BackgroundVideoElement autoPlay muted loop>
+          <source src={BackgroundVideo} type="video/mp4" />
+          Your browser does not support the video tag.
+        </BackgroundVideoElement>
+      </VideoContainer>
       <Navbar />
-    </div>
+    </HeaderContainer>
   );
 }
+
+const HeaderContainer = styled.header`
+  position: relative;
+  height: 100vh;
+  overflow: hidden;
+`;
+
+const VideoContainer = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+`;
+
+const BackgroundVideoElement = styled.video`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  position: absolute;
+  top: 0;
+  left: 0;
+  transform: scale(1.1);
+`;
