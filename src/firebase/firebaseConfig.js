bfcca0dac522234287b125415e7387e6f4f5fcfd
Maximilien Brunet <maximilien.brunet@me.com>
2025-01-23 23:15:11 +0100

    fix 2

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,7 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
-import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -10,13 +9,11 @@ const firebaseConfig = {
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 const storage = getStorage(app);
 const auth = getAuth(app);
 
-export { app, storage, auth, analytics };
+export { app, storage, auth };
