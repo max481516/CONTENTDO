757f790c3e881f28bc6f30fdc4b00b52175312fa
Maximilien Brunet <maximilien.brunet@me.com>
2025-01-24 19:02:12 +0100

    added env

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,19 +1,21 @@
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCrBJ_i1vbOFHG_X-hJxUpKY8FcTH-1Bnc",
-  authDomain: "contentdo-738fd.firebaseapp.com",
-  projectId: "contentdo-738fd",
-  storageBucket: "contentdo-738fd.firebasestorage.app",
-  messagingSenderId: "971303862574",
-  appId: "1:971303862574:web:3ad25bb2bacc6e512cbabf",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 
 const storage = getStorage(app);
+const analytics = getAnalytics(app);
 const auth = getAuth(app);
 
-export { app, storage, auth };
+export { app, storage, auth, analytics };
