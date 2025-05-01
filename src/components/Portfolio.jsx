44fdcfcb4a8531fb56964aac307ef41c361d6bd5
Maximilien Brunet <maximilien.brunet@me.com>
2025-05-01 11:50:19 +0400

    changed text in ContactUs

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -248,7 +248,7 @@ const PlayButton = styled.button`
 
 const PortfolioWrapper = styled.div`
   margin-top: calc(96rem / 16);
-  margin-bottom: 1rem;
+  margin-bottom: 6rem;
   padding: 0 116px;
 
   @media ${QUERIES.largeTabletAndDown} {
