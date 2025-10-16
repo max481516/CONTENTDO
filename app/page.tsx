e180f427b908068c0d3aec6fbf1d9a1894b1e052
max481516 <maximilien.brunet@me.com>
2025-10-16 12:51:39 +0400

    fix: try 2

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,11 @@
 import styled from "styled-components";
 import Header from "../src/components/Header.jsx";
 import Hero from "../src/components/Hero.jsx";
-import Portfolio from "../src/components/Portfolio.jsx";
+import dynamic from "next/dynamic";
+const Portfolio = dynamic(() => import("../src/components/Portfolio.jsx"), {
+  ssr: false,
+  loading: () => null,
+});
 import AboutUs from "../src/components/AboutUs.jsx";
 import PriceInfo from "../src/components/PriceInfo.jsx";
 import ContactUs from "../src/components/ContactUs.jsx";
