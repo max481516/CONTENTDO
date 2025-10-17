f1d08b57eba4e83bea7da5e64cb0e28b87978fa5
max481516 <maximilien.brunet@me.com>
2025-10-17 15:37:53 +0400

    fix: try 5

--- src/components/OrderForm.tsx
+++ src/components/OrderForm.tsx
@@ -152,7 +152,7 @@ export default function OrderForm() {
             console.log(`  ${key}: ${value}`);
           }
           
-          const response = await fetch("/", {
+          const response = await fetch("/netlify-forms.html", {
             method: "POST",
             headers: { "Content-Type": "application/x-www-form-urlencoded" },
             body: new URLSearchParams(formData as any).toString(),
